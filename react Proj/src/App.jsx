005df8aa2ea437de890fd8f8e085bff1da8a0d30
Ankith Y S <ankith.807348@gmail.com>
2025-10-31 01:37:24 +0530

    first commit

---
+++ react Proj/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div id="dad">
+        <h1 id="child1">Hello Boii</h1>
+        <h2 id="child2">Kya hai?</h2>
+      </div>
+      <div id="batija">
+        <h1>kya</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
