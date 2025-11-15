49bd751c8d3609c4026707f0fe97a00a4c32e427
Ankith Y S <ankith.807348@gmail.com>
2025-11-16 02:00:50 +0530

    Done how to use props ,and array in props

--- 4-Cards Proj/src/App.jsx
+++ 4-Cards Proj/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Card from "./components/Card";
+import User from "./components/User";
+
 
 const App = () => {
+  let arr=[6,5,3,34,62,85]
   return (
     <div className="parent">
 
       <Card />
-      <Card img=''/>
-
+      <User name= {arr[3]}/>
+      <User name='Ankit'/>
+      <User name='Manu'/>
     </div>
   );
 };
