882864602378a858bd399492e744dfc0388dead6
Ankith Y S <ankith.807348@gmail.com>
2025-11-17 14:59:06 +0530

    Mini Project Done

--- 4-Cards Proj/src/App.jsx
+++ 4-Cards Proj/src/App.jsx
@@ -1,17 +1,122 @@
-import React from "react";
 import Card from "./components/Card";
-import User from "./components/User";
+
 
 
 const App = () => {
-  let arr=[6,5,62,85]
-  return (
-    <div className="parent">
-      {arr.map(function(item){
-        return <h1>{item+3}</h1>;
-      })}
-    </div>
-  );
+const jobOpenings = [
+  {
+    brandLogo: "https://logo.clearbit.com/aws.amazon.com",
+    companyName: "AWS",
+    datePosted: "3 days ago",
+    post: "Cloud Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$70/hr",
+    location: "Seattle, USA"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/zoom.us",
+    companyName: "Zoom",
+    datePosted: "1 week ago",
+    post: "Video Infrastructure Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$85/hr",
+    location: "San Jose, USA"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/paypal.com",
+    companyName: "PayPal",
+    datePosted: "5 days ago",
+    post: "Full Stack Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$50/hr",
+    location: "Pune, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/airbnb.com",
+    companyName: "Airbnb",
+    datePosted: "2 weeks ago",
+    post: "Frontend Designer",
+    tag1: "Part Time",
+    tag2: "Junior Level",
+    pay: "$45/hr",
+    location: "London, UK"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/spotify.com",
+    companyName: "Spotify",
+    datePosted: "8 days ago",
+    post: "Backend Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$75/hr",
+    location: "Stockholm, Sweden"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/linkedin.com",
+    companyName: "LinkedIn",
+    datePosted: "10 days ago",
+    post: "Data Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$80/hr",
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/uber.com",
+    companyName: "Uber",
+    datePosted: "3 weeks ago",
+    post: "Mobile App Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$55/hr",
+    location: "Hyderabad, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/shopify.com",
+    companyName: "Shopify",
+    datePosted: "12 days ago",
+    post: "React Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$60/hr",
+    location: "Toronto, Canada"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/slack.com",
+    companyName: "Slack",
+    datePosted: "6 days ago",
+    post: "Product Designer",
+    tag1: "Part Time",
+    tag2: "Junior Level",
+    pay: "$48/hr",
+    location: "Remote"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/disney.com",
+    companyName: "Disney",
+    datePosted: "2 days ago",
+    post: "Video Graphics Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$90/hr",
+    location: "California, USA"
+  }
+];
+
+return(
+  <div className='parent'>
+    {jobOpenings.map(function(elem,index){
+      return  <div key={index}>
+         <Card company={elem.companyName} date={elem.datePosted}
+       brand={elem.brandLogo} loc={elem.location} pay={elem.pay}
+        post={elem.post} tag1={elem.tag1} tag2={elem.tag2} />
+      </div>
+    })}
+  </div>
+)
 };
 
 export default App;
--- 4-Cards Proj/src/components/Card.jsx
+++ 4-Cards Proj/src/components/Card.jsx
@@ -1,35 +1,39 @@
-import React from 'react'
-import {Bookmark} from 'lucide-react'
+import React from "react";
+import { Bookmark } from "lucide-react";
 
-const Card = () => {
+const Card = (props) => {
   return (
-          <div className="card">
-        {/* Card have 3 Sections */}
-        <div>
-                  <div className="top">
-          <img src="https://logo.clearbit.com/amazon.com" alt="" />
-          <button>Save <Bookmark  size={15}/></button>
+    <div className="card">
+      {/* Card have 3 Sections */}
+      <div>
+        <div className="top">
+          <img src={props.brand} alt="logo" />
+          <button>
+            Save <Bookmark size={15} />
+          </button>
         </div>
 
         <div className="center">
-          <h3>Amazon <span>5 days ago</span></h3>
-          <h2>Senior UI/UX Designer</h2>
-          <div className='tag'>
-            <h4>Part Time</h4>
-            <h4>Senior Level</h4>
+          <h3>
+            {props.company} <span>{props.date}</span>
+          </h3>
+          <h2>{props.post}</h2>
+          <div className="tag">
+            <h4>{props.tag1}</h4>
+            <h4>{props.tag2}</h4>
           </div>
         </div>
-        </div>
- 
-        <div className="bottom">
-            <div>
-              <h3>$120/hr</h3>
-              <p>Mumbai,India</p>
-            </div>
-            <button>Apply now</button>
-        </div>
       </div>
-  )
-}
 
-export default Card
+      <div className="bottom">
+        <div>
+          <h3>{props.pay}</h3>
+          <p>{props.loc}</p>
+        </div>
+        <button>Apply now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
