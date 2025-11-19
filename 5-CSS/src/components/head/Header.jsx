c29a76f7e1722b58394d39d8ac9854739447a564
Ankith Y S <ankith.807348@gmail.com>
2025-11-20 01:40:50 +0530

    Module CSS

--- 5-CSS/src/components/head/Header.jsx
+++ 5-CSS/src/components/head/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import styles from './Header.module.css'
+
 
 const Header = () => {
   return (
-    <div className='head'>
+    <div className={styles.head}>
         <h3>Me hu Gian </h3>
         <button>Ye bhi ek button hai</button>
         </div>
