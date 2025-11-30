5159a57e54870b11578882c4f3c513db339c53a6
Ankith Y S <ankith.807348@gmail.com>
2025-11-30 23:48:13 +0530

    Hero Text From Left Content

---
+++ 7-UI-Project/src/components/Section1/HeroText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+         <div className='p-5'>
+       <h3 className='mb-7 text-5xl leading-[1.1] font-bold'> Prospective <br /><span className='text-gray-600 rounded-full px-3 py-1'>Customer</span> <br /> Segmentation</h3>
+       <p className='text-xl font-medium text-gray-500'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas odio provident ut? Fuga excepturi praesentium, magnam aliquid veritatis fugit asperiores.</p>
+      </div>
+  )
+}
+
+export default HeroText
