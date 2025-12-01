97caf606f3361d19bbd5f026c92322e552e535f8
Ankith Y S <ankith.807348@gmail.com>
2025-12-01 23:51:39 +0530

    Nothing

--- 7-UI-Project/src/components/Section1/RightCard.jsx
+++ 7-UI-Project/src/components/Section1/RightCard.jsx
@@ -5,7 +5,7 @@ const RightCard = () => {
     <div className='h-full overflow-hidden relative w-80 rounded-4xl'>
         <img className='h-full w-full object-cover' src="https://images.unsplash.com/photo-1513128034602-7814ccaddd4e?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         <div className='absolute top-0 left-0 h-full w-full bg-yellow-300 '>
-
+    Ntg
         </div>
     </div>
   )
