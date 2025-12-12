4be743d421ee9d02837449f54def0e676448b4e4
Ankith Y S <ankith.807348@gmail.com>
2025-12-12 11:21:56 +0530

    useState-array-update

--- 10-useState-Advance/src/App.jsx
+++ 10-useState-Advance/src/App.jsx
@@ -2,17 +2,17 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  const [num, setNum] = useState({user:'Ankit', age:20})
+  const [num, setNum] = useState([10,20,30])
 
   const onChange=()=>{
-   const newNum={...num}
-   newNum.user='Manasa'
-   newNum.age=22;
-   setNum(newNum)
+    const newNum=[...num]
+    newNum.push(5)
+    setNum(newNum)
+
   }
   return (
     <div>
-      <h1>{num.user} ,{num.age}</h1>
+      <h1>{num}</h1>
       <button onClick={onChange}>Click Here </button>
     </div>
   )
