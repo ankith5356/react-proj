c48f0ba9df6259f6986338cc97a6d1e47f87f027
Ankith Y S <ankith.807348@gmail.com>
2026-02-07 00:14:14 +0530

    Gallery Project

--- 17-Gallery-Project/src/App.jsx
+++ 17-Gallery-Project/src/App.jsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Cards from './components/Cards'
+import Button from './components/Button'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [userData, setUserData] = useState([])
+
+    const [index, setIndex] = useState(1)
+
+const getData = async() => {
+  const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=10`)
+  setUserData(response.data)
+  console.log(response.data)
+}
+
+useEffect(function(){
+  getData()
+},[index])
+
+let printUserData = <h3 className='text-gray-400 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>LOADING....</h3>
+
+if(userData.length > 0){
+  printUserData = userData.map(function(elem,idx){
+
+    return <div key={idx}>
+      <Cards elem={elem} />
+    </div>
+  })
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='bg-black min-h-screen p-1 text-white'>
+
+     {/* Humme ab button se Data nahi chayie ab hum direct data utalete hai */}
+      {/* <button className='bg-green-400 px-10 py-5 m-5  rounded-2xl active:scale-95 text-2xl'
+      onClick={getData}
+      >Get Data</button> */}
+    
+      <div className=' flex h[82%] flex-wrap gap-3 p-4'>
+        {printUserData}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <Button  index={index} setIndex={setIndex} setUserData={setUserData} />
+
+    </div>
   )
 }
 
--- 17-Gallery-Project/src/components/Button.jsx
+++ 17-Gallery-Project/src/components/Button.jsx
@@ -5,6 +5,7 @@ const Button = (props) => {
     <div>
          <div className='flex justify-center gap-6 items-center p-4'>
         <button
+        style={{opacity : props.index == 1 ? 0.6 : 1}}
           className='bg-amber-400 text-sm cursor-pointer active:scale-90 text-black rounded px-4 py-2 font-semibold'
           onClick={()=>{
           if(props.index>1){
