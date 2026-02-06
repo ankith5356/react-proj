9d226b78d9596acbf4bb67f638c23a65fad17304
Ankith Y S <ankith.807348@gmail.com>
2026-02-06 23:59:54 +0530

    Components Section

---
+++ 17-Gallery-Project/src/components/Button.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Button = (props) => {
+  return (
+    <div>
+         <div className='flex justify-center gap-6 items-center p-4'>
+        <button
+          className='bg-amber-400 text-sm cursor-pointer active:scale-90 text-black rounded px-4 py-2 font-semibold'
+          onClick={()=>{
+          if(props.index>1){
+            props.setUserData([])
+            props.setIndex(props.index-1)
+          }
+        }}
+        >Prev
+        </button>
+
+        <h3 className='text-lg font-bold'>Page {props.index}</h3>
+
+        <button
+         className='bg-amber-400 text-sm cursor-pointer active:scale-90 text-black rounded px-4 py-2 font-semibold'
+         onClick={()=>{
+           props.setUserData([])
+          props.setIndex(props.index+1)
+        }}
+        >Next
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Button
